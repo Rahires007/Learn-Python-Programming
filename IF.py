87fde6984ce9f33593f0003919c5d2808b230ec8
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 16:46:30 +0530

    Create IF.py

---
+++ IF.py
@@ -0,0 +1,7 @@
+#IF conditional statement program
+Password=input("\nEnter your password....");
+if(Password=="Love"):
+    print("\nWelcome...")
+if(Password!="Love"):
+    print("\nPlease try again....")
+    
