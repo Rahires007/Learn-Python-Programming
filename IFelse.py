a196ff301c1fc0e73d97d649f70ea118e100a6b9
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 16:47:15 +0530

    Create IFelse.py

---
+++ IFelse.py
@@ -0,0 +1,6 @@
+#IF else conditional statement program
+Age=int(input("\nEnter your age...."))
+if(Age>=18):
+    print("\nYou are eligible for voting....")
+else:
+    print("\nYou are not eligible for voting....")
