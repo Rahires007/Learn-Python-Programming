04f75b154fae4df33092dd706c19c234128ff6c3
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 16:50:32 +0530

    Create Operation.py

---
+++ Operation.py
@@ -0,0 +1,7 @@
+#All arithmetic operation program
+Num1=int(input("\nEnter first number...."))
+Num2=int(input("\nEnter second number...."))
+print("\nAdd =",(Num1+Num2))
+print("\nSub =",(Num1-Num2))
+print("\nMulti =",(Num1*Num2))
+print("\nDiv =",(Num1/Num2))
