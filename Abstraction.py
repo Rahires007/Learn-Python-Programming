ff7a54978cef17a1a46f0534817fe87cf3bcad42
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 16:40:49 +0530

    Create Abstraction.py

---
+++ Abstraction.py
@@ -0,0 +1,11 @@
+#Data Abstraction program
+class Abstraction:
+    Num=10
+    Num1=20
+    Num2=30    
+    def Test():
+        print("\nNumber :-",A.Num)
+        print("\nNumber 1 :-",A.Num1)
+        print("\nNumber 2 :-",A.Num2)
+A=Abstraction
+A.Test()
