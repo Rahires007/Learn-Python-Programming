7ec99ab6bb93641a9bd2e5cf66938dc8f14baf6e
rahires007@gmail.com <rahires007@gmail.com>
2026-08-18 19:51:04 +0530

    Change in for.py

--- For.py
+++ For.py
@@ -1,4 +1,18 @@
+'''
+For is one type loop in python 
+When we have execute specific lines code repitedly then we use the Loop concept 
+Syntax :-
+for Varname in sequence:
+    Lines of code
+in :- It is special operator & Is also called as Membership operator 
+'''
 #For loop
 print("\n1 to 10 numbers...")
 for Num in range(1,11,1):
     print("\n",Num)
+print("-------------------------------------")
+#Traversing the string using For Loop
+Name="Ramesh"
+for Char in Name:
+    print(Char)
+print("-------------------------------------")
